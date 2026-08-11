6ad7d945b61f5a01165566af5f1e348a78c6437a
hassan4456 <39836344+hassan4456@users.noreply.github.com>
2026-08-12 01:48:12 +0300

    Delete agents/readability.py

--- agents/readability.py
+++
@@ -1,9 +0,0 @@
-def fix(pages):
-    fixed=[]
-    for p in pages:
-        txt=p.get('text','')
-        if len(txt.split())>12:
-            txt=' '.join(txt.split()[:12])+'.'
-        p['text']=txt
-        fixed.append(p)
-    return fixed
